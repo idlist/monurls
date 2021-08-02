ae3c63b91be1b00c3f6584162913146da284c619
i'DLisT <i.dlist@outlook.com>
2021-08-02 12:31:31 +0800

    feat: url-shortener UI

---
+++ frontend/src/layout.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import UrlShortener from './components/url-shortener'
+import 'layout.sass'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      monurls - My own Node.js URL shortener
+    </header>
+  )
+}
+
+const App = () => {
+  return (
+    <UrlShortener />
+  )
+}
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className='container'>
+        <App />
+      </div>
+    </>
+  )
+}
+
+export default Layout
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+export default defineConfig({
+  root: 'frontend',
+  server: {
+    port: 16666
+  },
+  plugins: [reactRefresh()]
+})
